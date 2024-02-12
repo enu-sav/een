363c879c9b91c97cdf716b52e4c8a6dc73b3198e
romanvasko <roman.vasko67@gmail.com>
2024-02-12 17:39:32 +0100

    Added sticky header & mobile menu.

--- web/themes/custom/een/assets/src/js/scripts.js
+++ web/themes/custom/een/assets/src/js/scripts.js
@@ -1,2 +1,45 @@
-((Drupal, settings) => {
-})(Drupal, drupalSettings);
+/**
+ * @file
+ * Global javascripts.
+ */
+
+if (typeof Drupal !== 'undefined') {
+  (function(Drupal, once) {
+    'use strict';
+
+    window.addEventListener('resize', function() {
+      if (this.resizeTO) clearTimeout(this.resizeTO);
+      this.resizeTO = setTimeout(function() {
+        // Trigger a custom event named 'resizeEnd'
+        var event = new Event('resizeEnd');
+        window.dispatchEvent(event);
+      }, 1);
+    });
+
+    /**
+     * Sticky header from 768px
+     */
+    document.addEventListener('DOMContentLoaded', function() {
+      window.addEventListener('scroll', function() {
+        var top = window.pageYOffset || document.documentElement.scrollTop;
+        var body = document.body;
+
+        if (top >= 180) {
+          body.classList.add('sticky-header');
+        } else {
+          body.classList.remove('sticky-header');
+        }
+      });
+    });
+
+    /**
+     * Navigation button
+     */
+    var navigationToggle = document.getElementsByClassName('mn__icon')[0];
+    navigationToggle.addEventListener('click', function() {
+      document.body.classList.toggle('navigation-expanded');
+      navigationToggle.classList.toggle('open-menu');
+    });
+
+  })(Drupal, once);
+}
