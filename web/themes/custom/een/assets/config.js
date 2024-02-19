5099c86230c7d6ea5d9fec4dd0985132849aab61
romanvasko <roman.vasko67@gmail.com>
2024-02-19 11:57:57 +0100

    Added paragraph gallery.

--- web/themes/custom/een/assets/config.js
+++ web/themes/custom/een/assets/config.js
@@ -1,10 +1,14 @@
 const config = {
   paths: {
     libs: [
-      // {
-      //   src: './node_modules/@splidejs/splide/dist/**/*',
-      //   dest: './dist/libs/splidejs'
-      // }
+      {
+        src: './node_modules/slick-carousel/slick/**/*',
+        dest: './dist/libs/slick'
+      },
+      {
+        src: './node_modules/lightgallery/**/*',
+        dest: './dist/libs/lightgallery'
+      },
     ]
   }
 }
---
+++ web/themes/custom/een/assets/src/js/paragraph.gallery.js
@@ -0,0 +1,62 @@
+/**
+ * @file
+ * "gallery" paragraph javascripts.
+ */
+(function ($, Drupal, once) {
+
+  "use strict";
+
+  Drupal.behaviors.paragraphGallery = {
+    attach: function (context, settings) {
+
+      once('paragraph-gallery', '.paragraph--type--gallery .field--name-field-media-image', context).forEach(function (item) {
+        if (item) {
+          lightGallery(item, {
+            plugins: [lgZoom, lgThumbnail, lgAutoplay, lgFullscreen, lgRotate],
+            selector: 'a',
+            download: false,
+            licenseKey: '605A035C-4289-4FC9-AC3D-15F950552E1B',
+            speed: 500,
+          });
+        }
+      });
+
+      once('paragraph-gallery', '.paragraph--type--gallery', context).forEach(function () {
+
+        var sliderFor = $('.field--name-field-images');
+
+        sliderFor.slick({
+          autoplay: false,
+          autoplaySpeed: 9999999999,
+          speed: 450,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: true,
+          infinite: true,
+          touchMove: false,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1
+              }
+            },
+          ]
+        });
+
+        $(window).resize(function(){
+          sliderFor[0].slick.refresh();
+        });
+      });
+
+    },
+  };
+
+})(jQuery, Drupal, once);
